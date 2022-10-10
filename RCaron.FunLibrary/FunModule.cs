2b58a7693dd68446ac6784c459caa4fd201f9519
Jan0660 <jan0660@tutanota.com>
2022-10-10 18:12:46 +0200

    feat(fun): do not run events on main motor

--- RCaron.FunLibrary/FunModule.cs
+++ RCaron.FunLibrary/FunModule.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Concurrent;
 using System.Linq.Expressions;
 using System.Reflection;
+using JetBrains.Annotations;
 using Microsoft.CSharp.RuntimeBinder;
 using RCaron.LibrarySourceGenerator;
 using Binder = Microsoft.CSharp.RuntimeBinder.Binder;
@@ -20,7 +21,7 @@ public partial class FunModule : IRCaronModule
         // var e = type.GetEvent("event");
         // var g = e.EventHandlerType;
         // var z = new Func<int>(() => 1);
-        
+
         real("balls");
     }
 
@@ -38,146 +39,148 @@ public partial class FunModule : IRCaronModule
     {
         public Motor Motor { get; }
         public CodeBlockToken CodeBlockToken { get; }
+
         public CodeBlockWrap(Motor motor, CodeBlockToken codeBlockToken)
         {
             Motor = motor;
             CodeBlockToken = codeBlockToken;
         }
-        internal object InvokeAsDelegateHelper(object[] args)
+
+        [UsedImplicitly]
+        internal object? InvokeAsDelegateHelper(object[] args)
         {
-            Console.WriteLine("wooo");
-            return "h";
-            // // Retrieve context and current runspace to ensure that we throw exception, if this is non-default runspace.
-            // ExecutionContext context = GetContextFromTLS();
-            // RunspaceBase runspace = (RunspaceBase)context.CurrentRunspace;
-            //
-            // List<object> rawResult = new List<object>();
-            // Pipe outputPipe = new Pipe(rawResult);
-            // InvokeWithPipe(
-            //     useLocalScope: true,
-            //     errorHandlingBehavior: ErrorHandlingBehavior.WriteToCurrentErrorPipe,
-            //     dollarUnder: dollarUnder,
-            //     input: null,
-            //     scriptThis: dollarThis,
-            //     outputPipe: outputPipe,
-            //     invocationInfo: null,
-            //     args: args);
-            // return GetRawResult(rawResult, wrapToPSObject: false);
+            using var m = MotorPool.GetAndPrepare(Motor);
+            var scope = new LocalScope();
+            scope.SetVariable("eventArgs", args);
+            m.Motor.BlockStack.Push(new(false, true, scope));
+            return m.Motor.RunCodeBlock(CodeBlockToken);
         }
     }
-    
-    
-        #region Delegates
-        // taken over from PowerShell
 
-        private static readonly ConcurrentDictionary<CodeBlockToken, ConcurrentDictionary<Type, Delegate>> s_delegateTable = new();
 
-        internal Delegate GetDelegate(CodeBlockToken codeBlock, Motor motor, Type delegateType)
+    #region Delegates
+
+    // taken over from PowerShell
+
+    private static readonly ConcurrentDictionary<CodeBlockToken, ConcurrentDictionary<Type, Delegate>> s_delegateTable =
+        new();
+
+    internal Delegate GetDelegate(CodeBlockToken codeBlock, Motor motor, Type delegateType)
+    {
+        ConcurrentDictionary<Type, Delegate> d;
+        if (!s_delegateTable.TryGetValue(codeBlock, out d))
         {
-            ConcurrentDictionary<Type, Delegate> d;
-            if (!s_delegateTable.TryGetValue(codeBlock, out d))
-            {
-                d = new();
-                s_delegateTable[codeBlock] = d;
-            }
-
-            Delegate ret;
-            ret = d.GetOrAdd(delegateType, () => CreateDelegate(codeBlock, motor, delegateType));
-            return ret;
-        }
-            // => s_delegateTable.GetOrCreateValue(this).GetOrAdd(delegateType, CreateDelegate);
-
-        /// <summary>
-        /// Get the delegate method as a call back.
-        /// </summary>
-        internal Delegate CreateDelegate(CodeBlockToken codeBlock, Motor motor, Type delegateType)
-        {
-            MethodInfo invokeMethod = delegateType.GetMethod("Invoke");
-            ParameterInfo[] parameters = invokeMethod.GetParameters();
-            if (invokeMethod.ContainsGenericParameters)
-            {
-                throw new("no code block delegate generic type");
-                // throw new ScriptBlockToPowerShellNotSupportedException(
-                //     "CantConvertScriptBlockToOpenGenericType",
-                //     null,
-                //     "AutomationExceptions",
-                //     "CantConvertScriptBlockToOpenGenericType",
-                //     delegateType);
-            }
-
-            // var codeBlockArg = Expression.Parameter(codeBlock.GetType());
-            // var motorArg = Expression.Parameter(motor.GetType());
-            var parameterExprs = new List<ParameterExpression>();
-            foreach (var parameter in parameters)
-            {
-                parameterExprs.Add(Expression.Parameter(parameter.ParameterType));
-            }
-
-            bool returnsSomething = !invokeMethod.ReturnType.Equals(typeof(void));
-
-            // Expression dollarUnderExpr;
-            // Expression dollarThisExpr;
-            // if (parameters.Length == 2 && !returnsSomething)
-            // {
-            //     // V1 was designed for System.EventHandler and not much else.
-            //     // The first arg (sender) was bound to $this, the second (e or EventArgs) was bound to $_.
-            //     // We do this for backwards compatibility, but we also bind the parameters (or $args) for
-            //     // consistency w/ delegates that take more or fewer parameters.
-            //     dollarUnderExpr = parameterExprs[1].Cast(typeof(object));
-            //     dollarThisExpr = parameterExprs[0].Cast(typeof(object));
-            // }
-            // else
-            // {
-            //     dollarUnderExpr = ExpressionCache.AutomationNullConstant;
-            //     dollarThisExpr = ExpressionCache.AutomationNullConstant;
-            // }
-            
-            var h = parameterExprs.Select(static p => Cast(p, typeof(object)));
-
-            Expression call = Expression.Call(
-                Expression.Constant(new CodeBlockWrap(motor, codeBlock)),
-                CodeBlockWrap_InvokeAsDelegateHelper,
-                // (object)
-                Expression.NewArrayInit(typeof(object), h));
-            if (returnsSomething)
-            {
-                call = DynamicExpression.Dynamic(
-                    // PSConvertBinder.Get(invokeMethod.ReturnType),
-                    Binder.Invoke(CSharpBinderFlags.None, invokeMethod.ReturnType, new CSharpArgumentInfo[]{CSharpArgumentInfo.Create(CSharpArgumentInfoFlags.None, null), }
-                    ),
-                    invokeMethod.ReturnType,
-                    call);
-            }
-
-            return Expression.Lambda(delegateType, call, parameterExprs).Compile();
+            d = new();
+            s_delegateTable[codeBlock] = d;
         }
 
+        Delegate ret;
+        ret = d.GetOrAdd(delegateType, () => CreateDelegate(codeBlock, motor, delegateType));
+        return ret;
+    }
+    // => s_delegateTable.GetOrCreateValue(this).GetOrAdd(delegateType, CreateDelegate);
 
-        private readonly static MethodInfo Convert_ChangeType = typeof(Convert).GetMethod("Convert");
-        internal static Expression Cast(Expression expr, Type type)
+    /// <summary>
+    /// Get the delegate method as a call back.
+    /// </summary>
+    internal Delegate CreateDelegate(CodeBlockToken codeBlock, Motor motor, Type delegateType)
+    {
+        MethodInfo invokeMethod = delegateType.GetMethod("Invoke");
+        ParameterInfo[] parameters = invokeMethod.GetParameters();
+        if (invokeMethod.ContainsGenericParameters)
         {
-            if (expr.Type == type)
-            {
-                return expr;
-            }
-
-            if ((
-                    // todo(current)
-                    // expr.Type.IsFloating() || 
-                 expr.Type == typeof(decimal)) && type.IsPrimitive)
-            {
-                // Convert correctly handles most "primitive" conversions for PowerShell,
-                // but it does not correctly handle floating point.
-                expr = Expression.Call(
-                    Convert_ChangeType,
-                    Expression.Convert(expr, typeof(object)),
-                    Expression.Constant(type, typeof(Type)));
-            }
-
-            return Expression.Convert(expr, type);
+            throw new("no code block delegate generic type");
+            // throw new ScriptBlockToPowerShellNotSupportedException(
+            //     "CantConvertScriptBlockToOpenGenericType",
+            //     null,
+            //     "AutomationExceptions",
+            //     "CantConvertScriptBlockToOpenGenericType",
+            //     delegateType);
         }
-        
-        private MethodInfo CodeBlockWrap_InvokeAsDelegateHelper= typeof(CodeBlockWrap).GetMethod("InvokeAsDelegateHelper", BindingFlags.NonPublic | BindingFlags.Instance);
 
-        #endregion
+        // var codeBlockArg = Expression.Parameter(codeBlock.GetType());
+        // var motorArg = Expression.Parameter(motor.GetType());
+        var parameterExprs = new List<ParameterExpression>();
+        foreach (var parameter in parameters)
+        {
+            parameterExprs.Add(Expression.Parameter(parameter.ParameterType));
+        }
+
+        bool returnsSomething = !invokeMethod.ReturnType.Equals(typeof(void));
+
+        // Expression dollarUnderExpr;
+        // Expression dollarThisExpr;
+        // if (parameters.Length == 2 && !returnsSomething)
+        // {
+        //     // V1 was designed for System.EventHandler and not much else.
+        //     // The first arg (sender) was bound to $this, the second (e or EventArgs) was bound to $_.
+        //     // We do this for backwards compatibility, but we also bind the parameters (or $args) for
+        //     // consistency w/ delegates that take more or fewer parameters.
+        //     dollarUnderExpr = parameterExprs[1].Cast(typeof(object));
+        //     dollarThisExpr = parameterExprs[0].Cast(typeof(object));
+        // }
+        // else
+        // {
+        //     dollarUnderExpr = ExpressionCache.AutomationNullConstant;
+        //     dollarThisExpr = ExpressionCache.AutomationNullConstant;
+        // }
+
+        var h = parameterExprs.Select(static p => Cast(p, typeof(object)));
+
+        Expression call = Expression.Call(
+            Expression.Constant(new CodeBlockWrap(motor, codeBlock)),
+            CodeBlockWrap_InvokeAsDelegateHelper,
+            // (object)
+            Expression.NewArrayInit(typeof(object), h));
+        if (returnsSomething)
+        {
+            var variable = Expression.Variable(typeof(object), "returnValue");
+            var block = Expression.Block(
+                new[] { variable },
+                Expression.Assign(variable, call),
+                Expression.Convert(variable, invokeMethod.ReturnType));
+            call = block;
+            // call = DynamicExpression.Dynamic(
+            //     // PSConvertBinder.Get(invokeMethod.ReturnType),
+            //     Binder.Invoke(CSharpBinderFlags.None, null,
+            //         new CSharpArgumentInfo[] { CSharpArgumentInfo.Create(CSharpArgumentInfoFlags.None, null), }
+            //     ),
+            //     invokeMethod.ReturnType,
+            //     call);
+        }
+
+        Console.WriteLine(delegateType.ToString());
+        return Expression.Lambda(delegateType, call, parameterExprs).Compile();
+    }
+
+
+    private readonly static MethodInfo Convert_ChangeType = typeof(Convert).GetMethod("Convert");
+
+    internal static Expression Cast(Expression expr, Type type)
+    {
+        if (expr.Type == type)
+        {
+            return expr;
+        }
+
+        if ((
+                // todo(current)
+                // expr.Type.IsFloating() || 
+                expr.Type == typeof(decimal)) && type.IsPrimitive)
+        {
+            // Convert correctly handles most "primitive" conversions for PowerShell,
+            // but it does not correctly handle floating point.
+            expr = Expression.Call(
+                Convert_ChangeType,
+                Expression.Convert(expr, typeof(object)),
+                Expression.Constant(type, typeof(Type)));
+        }
+
+        return Expression.Convert(expr, type);
+    }
+
+    private MethodInfo CodeBlockWrap_InvokeAsDelegateHelper =
+        typeof(CodeBlockWrap).GetMethod("InvokeAsDelegateHelper", BindingFlags.NonPublic | BindingFlags.Instance);
+
+    #endregion
 }
---
+++ RCaron.FunLibrary/MotorPool.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Concurrent;
+
+namespace RCaron.FunLibrary;
+
+public static class MotorPool
+{
+    public static List<MotorPoolItem> Pool = new();
+
+    public static MotorPoolItem GetAndPrepare(Motor parent)
+    {
+        void Prepare(Motor motor)
+        {
+            motor.Functions = parent.Functions;
+            motor.Lines = parent.Lines;
+            motor.Modules = parent.Modules;
+            motor.Raw = parent.Raw;
+            motor.ClassDefinitions = parent.ClassDefinitions;
+            motor.FileScope = parent.FileScope;
+            motor.GlobalScope.SetVariable("parentMotor", parent);
+        }
+        Motor res;
+        for (var i = 0; i < Pool.Count; i++)
+        {
+            if (Pool[i].InUse == false)
+            {
+                Pool[i].InUse = true;
+                return Pool[i];
+            }
+        }
+
+        res = new Motor(new RCaronRunnerContext(parent.Raw, parent.Lines, parent.ClassDefinitions));
+        Prepare(res);
+        var item = new MotorPoolItem(res) { InUse = true };
+        Pool.Add(item);
+        return item;
+    }
+}
+
+/// <summary>
+/// Not actually disposable but it's a good way to make sure the motor is returned to the pool
+/// </summary>
+/// <param name="Motor"></param>
+/// <param name="InUse"></param>
+public record MotorPoolItem(Motor Motor) : IDisposable
+{
+    public bool InUse { get; set; }
+    public void Dispose()
+    {
+        this.InUse = false;
+    }
+}
