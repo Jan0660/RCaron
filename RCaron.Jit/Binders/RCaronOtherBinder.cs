bbcfe87004fbf92e5a592944b65547fda02c643b
Jan0660 <jan0660@tutanota.com>
2023-02-19 10:30:14 +0100

    feat: class functions with arguments working properly

--- RCaron.Jit/Binders/RCaronOtherBinder.cs
+++ RCaron.Jit/Binders/RCaronOtherBinder.cs
@@ -27,56 +27,9 @@ public class RCaronOtherBinder : DynamicMetaObjectBinder
         // RCaron function
         var func = CompiledContext.GetFunction(Name);
         if (func != null)
-        {
-            Expression?[]? exps = null;
-            if (func.OriginalFunction.Arguments is not null)
-            {
-                var arguments = func.OriginalFunction.Arguments;
-                if (CallInfo.ArgumentCount > arguments.Length)
-                    throw RCaronException.LeftOverPositionalArgument();
-                exps = new Expression?[arguments.Length];
-                for (var i = 0; i < CallInfo.ArgumentCount - CallInfo.ArgumentNames.Count; i++)
-                    exps[i] = args[i].Expression;
-                for (var index = 0; index < CallInfo.ArgumentNames.Count; index++)
-                {
-                    var namedArg = CallInfo.ArgumentNames[index];
-                    var found = false;
-                    for (var i = 0; i < arguments.Length; i++)
-                        if (arguments[i].Name.Equals(namedArg,
-                                StringComparison.InvariantCultureIgnoreCase))
-                        {
-                            exps[i] = args[index + CallInfo.ArgumentCount - CallInfo.ArgumentNames.Count]
-                                .Expression;
-                            found = true;
-                            break;
-                        }
-
-                    if (!found)
-                        throw RCaronException.NamedArgumentNotFound(namedArg);
-                }
-
-                // assign default values
-                for (var i = 0; i < exps.Length; i++)
-                    if (exps[i] == null)
-                    {
-                        if (!arguments[i].DefaultValue?.Equals(RCaronInsideEnum.NoDefaultValue) ?? true)
-                            exps[i] = Expression.Constant(arguments[i].DefaultValue);
-                        else
-                            throw RCaronException.ArgumentsLeftUnassigned(arguments[i].Name);
-                    }
-            }
-
-            return new DynamicMetaObject(
-                Expression.Call(Expression.Constant(func),
-                        typeof(CompiledFunction).GetMethod(nameof(CompiledFunction.Invoke))!,
-                        exps == null
-                            ? Expression.Constant(null, typeof(object[]))
-                            : Expression.NewArrayInit(
-                                typeof(object), exps.Select(x => x!.EnsureIsType(typeof(object))!)))
-                    .EnsureIsType(ReturnType),
+            return Shared.DoFunction(func, target, args, Name, CallInfo, typeof(object),
                 BindingRestrictions.GetExpressionRestriction(Expression.Equal(target.Expression,
                     Expression.Constant(EnsureSameOrigin))));
-        }
 
         // module
         if (CompiledContext.FileScope.Modules != null)
