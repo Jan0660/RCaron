bc48f2b97c1f726f7b815c4c69a0bafa6041e31d
Jan0660 <jan0660@tutanota.com>
2023-03-05 10:55:36 +0100

    feat(LanguageServer): do not write logs to a log file

--- RCaron.LanguageServer/Program.cs
+++ RCaron.LanguageServer/Program.cs
@@ -29,7 +29,7 @@ namespace RCaron.LanguageServer
 
             Log.Logger = new LoggerConfiguration()
                         .Enrich.FromLogContext()
-                        .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
+                        // .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
                         .MinimumLevel.Verbose()
                         .CreateLogger();
             
