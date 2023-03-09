400132de12a1d879fe61a6806df60137fcc3563b
Jan0660 <jan0660@tutanota.com>
2023-03-09 16:23:08 +0100

    fix: ParsingErrorThrowHandler return true in AllowsExecution, because parsing would fail anyway and we don't want to have to create a new instance everytime

--- RCaron/Parsing/ParsingErrorThrowHandler.cs
+++ RCaron/Parsing/ParsingErrorThrowHandler.cs
@@ -2,14 +2,11 @@
 
 public class ParsingErrorThrowHandler : IParsingErrorHandler
 {
-    public bool Threw { get; private set; }
-
     public bool Handle(ParsingException exception)
     {
-        Threw = true;
         throw exception;
     }
 
     public bool AllowsExecution()
-        => !Threw;
+        => true;
 }
